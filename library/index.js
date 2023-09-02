067fc7e6eb8b713676b9f516e2bb57ce79fef75b
Dzmitry Yushkevich <axule12@gmail.com>
2023-09-02 23:46:56 +0300

    feat: add slider for favorites block

--- library/index.js
+++ library/index.js
@@ -1,5 +1,6 @@
 console.log('1. Вёрстка соответствует макету. Ширина экрана 768px +26\n 2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n На ширине экрана 768рх реализовано адаптивное меню +12\n Общая оценка 50 баллов')
 
+//анимация меню
 function showMenu(){
   
   document.getElementById('mobile-menu').classList.remove('header__nav-block-close');
@@ -8,6 +9,8 @@ function closeMenu(){
   document.getElementById('mobile-menu').classList.toggle('header__nav-block-close');
 }
 
+
+//анимация слайдера
 const SLIDER = document.querySelector('#carousel');
 const LEFT_ARROW = document.querySelector('.carret__left');
 const RIGHT_ARROW = document.querySelector('.carret__right');
@@ -37,6 +40,71 @@ RIGHT_ARROW.addEventListener('click', function(){
   INDICATOR_LIST.children[sectionIndex].classList.add('btn_active')
   SLIDER.style.transform = 'translate(' + (sectionIndex) * -465 + 'px)';
 })
+
+
+//анимация выбора книг
+
+
+const FILTER = document.querySelectorAll('[data-tab]');
+console.log(FILTER);
+const LIST_BOOK = document.querySelectorAll('[data-tab-content]');
+console.log(LIST_BOOK);
+
+FILTER.forEach(function(item){
+
+  item.addEventListener('click',function(){
+    LIST_BOOK.forEach(function(item){
+      item.classList.remove('hide__list-book');
+      item.classList.add('hide-zero__list-book');
+      setTimeout(() => item.classList.add('none__list-book'), 2000)
+      setTimeout(() => item.classList.remove('block__list-book'), 2000);
+    });
+      const SELECT_BOOK = document.querySelector('#' + this.dataset.tab);
+      setTimeout(() => SELECT_BOOK.classList.add('block__list-book'), 2000);
+      setTimeout(() => SELECT_BOOK.classList.add('hide__list-book'), 2500);
+  })
+})
+
+
+// const LIST_BOOK = document.querySelectorAll('.favorites__list-book');
+// const FILTER = document.querySelectorAll('.favorites__list input');
+
+// FILTER[1].addEventListener('click', function () {
+//   LIST_BOOK[0].classList.remove('hide__list-book');
+//   LIST_BOOK[0].classList.add('hide-zero__list-book');
+
+//   setTimeout(() => LIST_BOOK[0].classList.add('none__list-book'), 2000)
+//   setTimeout(() => LIST_BOOK[0].classList.remove('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[1].classList.add('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[1].classList.add('hide__list-book'), 2500);
+// })
+// FILTER[2].addEventListener('click', function () {
+//   LIST_BOOK[1].classList.remove('hide__list-book');
+//   LIST_BOOK[1].classList.add('hide-zero__list-book');
+
+//   setTimeout(() => LIST_BOOK[1].classList.add('none__list-book'), 2000)
+//   setTimeout(() => LIST_BOOK[1].classList.remove('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[2].classList.add('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[2].classList.add('hide__list-book'), 2500);
+// })
+// FILTER[3].addEventListener('click', function () {
+//   LIST_BOOK[2].classList.remove('hide__list-book');
+//   LIST_BOOK[2].classList.add('hide-zero__list-book');
+
+//   setTimeout(() => LIST_BOOK[2].classList.add('none__list-book'), 2000)
+//   setTimeout(() => LIST_BOOK[2].classList.remove('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[3].classList.add('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[3].classList.add('hide__list-book'), 2500);
+// })
+// FILTER[0].addEventListener('click', function () {
+//   LIST_BOOK[3].classList.remove('hide__list-book');
+//   LIST_BOOK[3].classList.add('hide-zero__list-book');
+
+//   setTimeout(() => LIST_BOOK[3].classList.add('none__list-book'), 2000)
+//   setTimeout(() => LIST_BOOK[3].classList.remove('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[0].classList.add('block__list-book'), 2000);
+//   setTimeout(() => LIST_BOOK[0].classList.add('hide__list-book'), 2500);
+// })
 
 
 // анимация замены картинки при клике
