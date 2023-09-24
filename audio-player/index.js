97ac61430eaf12656b20f2b26907a250996c8730
Dzmitry Yushkevich <axule12@gmail.com>
2023-09-24 22:55:17 +0300

    feat: add function secondToTime

--- audio-player/index.js
+++ audio-player/index.js
@@ -36,12 +36,44 @@ let pauseBtn = document.getElementById('pauseBtn');
 let trackLine = document.getElementById('track-line');
 let nextBtn = document.getElementById('nextBtn');
 let prevBtn = document.getElementById('prevBtn');
+let lastTime = document.getElementById('last-time');
+let totalTime = document.getElementById('total-time');
 let track; // Переменная с индексом трека
  
 // Событие перед загрузкой страницы
 window.onload = function() {
     track = 0; // Присваиваем переменной ноль
 }
+
+/*Функция приведения секунд к минутам*/
+function secondsToTime(time){
+  var h = Math.floor(time / (60 * 60)),
+      dm = time % (60 * 60),
+      m = Math.floor(dm / 60),
+      ds = dm % 60,
+      s = Math.ceil(ds);
+  if (s === 60) {
+      s = 0;
+      m = m + 1;
+  }
+  if (s < 10) {
+      s = '0' + s;
+  }
+  if (m === 60) {
+      m = 0;
+      h = h + 1;
+  }
+  if (m < 10) {
+      m = '0' + m;
+  }
+  if (h === 0) {
+      fulltime = m + ':' + s;
+  } else {
+      fulltime = h + ':' + m + ':' + s;
+  }
+  return fulltime;
+}
+
 
 /*скрыть показать кнопки паузы и проигрывания*/
 function toggleBtn() {
@@ -71,6 +103,8 @@ playBtn.addEventListener("click", function () {
     let audioTime = Math.round(audio.currentTime);
     let audioLength = Math.round(audio.duration)
     trackLine.style.width = (audioTime * 100) / audioLength + '%';
+    lastTime.innerHTML = secondsToTime(audio.currentTime);
+    totalTime.innerHTML = secondsToTime(audio.duration);
 
     if (audioTime == audioLength && track < numberSong) {
         track++; 
