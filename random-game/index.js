60a28b17af7ecc6f84b2098f033d63f262858632
Dzmitry Yushkevich <axule12@gmail.com>
2023-10-07 23:25:29 +0300

    feat: add level game

--- random-game/index.js
+++ random-game/index.js
@@ -1,49 +1,49 @@
 //переменные для работы с графикой
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
 
 
-let bumpAudio = document.getElementById('bump-audio');
+var bumpAudio = document.getElementById('bump-audio');
 
 
 //радиус мяча
-let ballRadius = 10;
+var ballRadius = 10;
 
 //для создания ракетки
 
-let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleX = (canvas.width - paddleWidth) / 2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
 //начальные координаты мяча
-let x = canvas.width / 2;
-let y = canvas.height - 20;
+var x = canvas.width / 2;
+var y = canvas.height - 20;
 //для смещения мяча
-let dx = 0;
-let dy = 0;
+var dx = 0;
+var dy = 0;
 
 //состояние клавиш
-let rightPressed = false;
-let leftPressed = false;
-let spacePressed = false;
+var rightPressed = false;
+var leftPressed = false;
+var spacePressed = false;
 
 
 
 
 //переменные для кирпичей
-let brickRowCount = 3;
-let brickColumnCount = 5;
-let brickWidth = 75;
-let brickHeight = 20;
-let brickPadding = 10;
-let brickOffsetTop = 30;
-let brickOffsetLeft = 30;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
 
-let brick = [];
-for (let c = 0; c < brickColumnCount; c++) {
+var brick = [];
+for (var c = 0; c < brickColumnCount; c++) {
   brick[c] = [];
-  for (let r = 0; r < brickRowCount; r++) {
+  for (var r = 0; r < brickRowCount; r++) {
     brick[c][r] = { x: 0, y: 0, status: 1 };
 
   }
@@ -51,18 +51,21 @@ for (let c = 0; c < brickColumnCount; c++) {
 }
 
 //очки
-let score = 0;
+var score = 0;
+var totalScore = 0;
 
-let lives = 3
+var lives = 3
+
+var level = 0;
 
 
 
 function drawBricks() { //рисуем кирпичи
-  for (let c = 0; c < brickColumnCount; c++) {
-    for (let r = 0; r < brickRowCount; r++) {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
       if (brick[c][r].status == 1) {
-        let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-        let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
         brick[c][r].x = brickX;
         brick[c][r].y = brickY;
         ctx.beginPath();
@@ -99,7 +102,8 @@ function draw() { //сама игра
   drawBricks();
   collisionDetection();
   drawScore();
-  drawLives()
+  drawLives();
+  drawLevel()
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
@@ -109,20 +113,20 @@ function draw() { //сама игра
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
       racketBump();
-      
+
     } else {
-      lives --;
+      lives--;
       if (!lives) {
-        alert("Game Over\n" + 
-        'Score: ' + score);
+        alert("Game Over\n" +
+          'Score: ' + totalScore);
         document.location.reload();
-        
+
       } else {
         //стартовая позиция
         x = canvas.width / 2;
         y = canvas.height - 20;
         dx = 0;
-        dy = 0   ;
+        dy = 0;
         paddleX = (canvas.width - paddleWidth) / 2;
       }
 
@@ -144,10 +148,10 @@ function draw() { //сама игра
 }
 
 //звуки
-function racketBump(){
+function racketBump() {
   bumpAudio.src = "./assets/audio/racket.mp3";
 }
-function brickBump(){
+function brickBump() {
   bumpAudio.src = "./assets/audio/brick.mp3";
 }
 //слушаем событие нажатия и отпускания кнопки, выполняем функции
@@ -172,27 +176,27 @@ function keyUpHandler(e) {
 }
 
 document.addEventListener("mousemove", mouseMoveHandler, false);
-function mouseMoveHandler(e){
-  let relativeX = e.clientX - canvas.offsetLeft;
-  if (relativeX > 0 && relativeX < canvas.width){
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
     paddleX = relativeX - paddleWidth / 2;
   }
 }
 
 //функция старта игры
 function startGame(e) {
-  if(e.keyCode == 32) {
-    dx = 2;
-    dy = -2;
+  if (e.keyCode == 32) {
+    dx = 3 + level;
+    dy = -3 - level;
   }
-  
+
 }
 
 
 function collisionDetection() { //отталкиваемся от кирпича
-  for (let c = 0; c < brickColumnCount; c++) {
-    for (let r = 0; r < brickRowCount; r++) {
-      let b = brick[c][r];
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      var b = brick[c][r];
       if (b.status == 1) {
         if (x > b.x &&
           x < b.x + brickWidth &&
@@ -203,10 +207,26 @@ function collisionDetection() { //отталкиваемся от кирпича
           b.status = 0;
           brickBump();
           score++;
+          totalScore++
           if (score == brickRowCount * brickColumnCount) {
-            alert("YOU WIN, CONGRATULATIONS!\n" + 
-            'Score: ' + score);
-            document.location.reload();
+            // alert("YOU WIN, CONGRATULATIONS!\n" + 
+            // 'Score: ' + score);
+            score = 0;
+            lives++;
+            level++
+            x = canvas.width / 2;
+            y = canvas.height - 20;
+            dx = 0;
+            dy = 0;
+            for (var c = 0; c < brickColumnCount; c++) {
+              brick[c] = [];
+              for (var r = 0; r < brickRowCount; r++) {
+                brick[c][r] = { x: 0, y: 0, status: 1 };
+
+              }
+
+            }
+            // document.location.reload(); //перезапустить игру
           }
         }
       }
@@ -218,7 +238,7 @@ function collisionDetection() { //отталкиваемся от кирпича
 function drawScore() {
   ctx.font = '16px Arial';
   ctx.fillStyle = "#0095DD";
-  ctx.fillText('Score: ' + score, 8, 20);
+  ctx.fillText('Score: ' + totalScore, 8, 20);
 }
 
 function drawLives() {
@@ -226,5 +246,9 @@ function drawLives() {
   ctx.fillStyle = '0095DD';
   ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
 }
-
+function drawLevel() {
+  ctx.font = '16px Arial';
+  ctx.fillStyle = '0095DD';
+  ctx.fillText("Level: " + level , canvas.width - 275, 20);
+}
 draw();
